8425aa171aa2e781637a0d4eab9abc351637e195
Amanda Beltoso <amandabeltoso@hotmail.com>
2017-10-23 19:32:15 -0300

    Teste commit

--- Programa.java
+++ Programa.java
@@ -2,6 +2,6 @@ public class random{
 
   public static void public static void main(String[] args) {
     System.out.println(" " + Math.random);
-    System.out.println("Tal");
+    System.out.println("Nada");
   }
 }
